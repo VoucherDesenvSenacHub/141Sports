ec8bf9321e8a7d6f89d0d241f0887db5040dbd0b
garffield <gabrielfranklinbarcellos@gmail.com>
2024-11-27 09:28:15 -0300

    model Funcionario 50%

--- model/Funcionario.php
+++ model/Funcionario.php
@@ -2,7 +2,7 @@
 
 require 'Usuario.php';
 class Funcionario extends Usuario {
-    public $adminVerificacao = Boolean;
+    public $adminVerificacao = False;
     //não tenho certeza se isso funciona
     //falta os metodos de funcionarios que ainda precisa ver quais são
 }
